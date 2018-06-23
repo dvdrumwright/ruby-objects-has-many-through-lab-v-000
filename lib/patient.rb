5ec3f9baedab0da0332df76dc923949e819de9db
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-06-23 17:28:23 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -17,7 +17,7 @@ class Patient
   
   
   def new_appointment(doctor,date)
-    appointment = Appointment.new(doctor,self,date)
+      Appointment.new(doctor,self,date)
   end 
   
   def appointments 
