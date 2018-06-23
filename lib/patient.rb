c86e04cf0842850450de03b9f2437f66c6fdb27f
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-06-23 17:23:37 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,2 +1,27 @@
-class Patient 
+class Patient
+  
+  @@all = []
+   
+  attr_accessor :name, :doctor  
+   
+  def initialize(name,doctor)
+     @name = name 
+     @doctor = doctor
+     @@all << self 
+  end 
+  
+   
+  def self.all
+      @@all
+  end
+  
+  
+  def new_appointment(doctor,date)
+    Appointment.new(doctor,self,date)
+  end 
+  
+  def appointments 
+      Appointment.all.select { |appointment| appointment.patient == self }
+  end 
+  
 end 
